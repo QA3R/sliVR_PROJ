cec995b3308782ffb6b3d778362a50a1b5f63415
QA3R <46014750+QA3R@users.noreply.github.com>
2020-10-22 19:17:36 -0500

    Added VideoTestScene
    Additions:
    - Can have an array of videos clips
    - Can pass a method to play a particular clip based on its array index
    
    Notes:
    - There's an option to make the camera rotate slightly around the video plane, but I'm not sure if it really does much.. :/

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity;
+    [SerializeField]
+    private float minVal = 90f;
+    [SerializeField]
+    private float maxVal = 90f;
+    private float xRotation = 0f;
+    private float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float verticalRotation = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= verticalRotation;
+        xRotation = Mathf.Clamp(xRotation, minVal, maxVal);
+
+        //transform.Translate(new Vector3(horizontalRotation, verticalRotation, 0));
+
+        yRotation += horizontalRotation;
+        yRotation = Mathf.Clamp(yRotation, minVal, maxVal);
+
+        transform.localRotation = Quaternion.Euler (xRotation, yRotation, 0f);
+    }
+}
---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    [SerializeField]
+    private VideoClip[] vidClips;
+
+    [SerializeField]
+    public GameObject videoPlayerObj;
+    private VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //videoPlayerObj = GameObject.Find("ScreenSpace");
+        videoPlayer = videoPlayerObj.GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            PlayVideoClip(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            PlayVideoClip(1);
+        }
+    }
+
+    public void PlayVideoClip (int clipID)
+    {
+        videoPlayer.clip = vidClips[clipID];
+        videoPlayer.Play();
+    }
+
+}
