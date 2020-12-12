02a118ac575e678acc14406ba12a0e09cffd7d27
QA3R <46014750+QA3R@users.noreply.github.com>
2020-12-11 20:52:57 -0500

    Quit game and Main menu buttons
    Notes:
    - Quit Game button functionality added
    - Main menu not available (causes issues with loading into the game a second time)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GameManager : MonoBehaviour
+{
+    private VideoPlayer _videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _videoPlayer = GameObject.FindObjectOfType<VideoPlayer>();
+    }
+   
+    public void PauseGame()
+    {
+        _videoPlayer.Pause();
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        _videoPlayer.Play();
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game quit sucessfully");
+    }
+}
